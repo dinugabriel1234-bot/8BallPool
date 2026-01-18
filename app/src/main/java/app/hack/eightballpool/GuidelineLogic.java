2c00ae62f3d0c6cd38aa3332597ec81c768bf4ac
Gabriel <dinugabriel1234@gmail.com>
2026-01-18 11:28:02 +0200

    Update GuidelineLogic.java

--- app/src/main/java/app/hack/eightballpool/GuidelineLogic.java
+++ app/src/main/java/app/hack/eightballpool/GuidelineLogic.java
@@ -1,23 +1,28 @@
 package app.hack.eightballpool;
 
+import java.util.List;
+
 public class GuidelineLogic {
 
     private float cueX, cueY;
-    private float[] targetX, targetY;
+    private List<Float> ballX, ballY;
+    private List<Float> pocketX, pocketY;
 
-    public void updatePositions(float cueX, float cueY, float[] targetX, float[] targetY) {
+    public void updatePositions(float cueX, float cueY,
+                                List<Float> ballX, List<Float> ballY,
+                                List<Float> pocketX, List<Float> pocketY) {
         this.cueX = cueX;
         this.cueY = cueY;
-        this.targetX = targetX;
-        this.targetY = targetY;
+        this.ballX = ballX;
+        this.ballY = ballY;
+        this.pocketX = pocketX;
+        this.pocketY = pocketY;
     }
 
-    public Line[] calculateLines() {
-        if (targetX == null || targetY == null) return new Line[0];
-        Line[] lines = new Line[targetX.length];
-        for (int i = 0; i < targetX.length; i++) {
-            lines[i] = new Line(cueX, cueY, targetX[i], targetY[i]);
-        }
-        return lines;
-    }
+    public float getCueX() { return cueX; }
+    public float getCueY() { return cueY; }
+    public List<Float> getBallX() { return ballX; }
+    public List<Float> getBallY() { return ballY; }
+    public List<Float> getPocketX() { return pocketX; }
+    public List<Float> getPocketY() { return pocketY; }
 }
