4a272ab69aefdc32eea41b50d05704f9aa0e30ea
Gabriel <dinugabriel1234@gmail.com>
2026-01-18 10:57:12 +0200

    Create Line.java

---
+++ app/src/main/java/app/hack/eightballpool/Line.java
@@ -0,0 +1,12 @@
+package app.hack.eightballpool;
+
+public class Line {
+    public float startX, startY, endX, endY;
+
+    public Line(float startX, float startY, float endX, float endY) {
+        this.startX = startX;
+        this.startY = startY;
+        this.endX = endX;
+        this.endY = endY;
+    }
+}
